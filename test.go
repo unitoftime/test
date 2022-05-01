ec5d266accf5d281cc0a7b2ef32a7c8bcac7babd
Jacob Stewart <2606873+unitoftime@users.noreply.github.com>
2022-05-01 10:58:17 -0400

    removed todo

--- test.go
+++ test.go
@@ -2,10 +2,7 @@ package main
 
 import "fmt"
 
-// TODO some multiple line description of a todo item
-//      there's like a ton of work to do.
-//      and it's really long to describe
 func main() {
-	// BUG - Some bug description
+
 	fmt.Println("hello")
 }
